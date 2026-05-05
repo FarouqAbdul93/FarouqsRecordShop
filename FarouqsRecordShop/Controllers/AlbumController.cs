4dcb814e9b95547d3e7b0d54fe27829148f316fe
Farouq <farouqha93@gmail.com>
2026-05-05 11:38:46 +0100

    feat: add album controller layer

---
+++ FarouqsRecordShop/Controllers/AlbumController.cs
@@ -0,0 +1,17 @@
+﻿using FarouqsRecordShop.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FarouqsRecordShop.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlbumController : ControllerBase
+    {
+        private readonly IAlbumService _service;
+
+        public AlbumController(IAlbumService service)
+        {
+            _service = service;
+        }
+    }
+}
